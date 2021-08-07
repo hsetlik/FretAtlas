c1faf40009f193944f05f3552cdae274affbb23b
Hayden Setlik <haydensetlik@gmail.com>
2021-08-07 16:22:24 -0600

    pushing to github

--- ContentView.swift
+++ ContentView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Test")
-        FretboardView()
+        VStack{
+            FretboardView()
+        }
     }
 }
 
